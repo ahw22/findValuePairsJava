16ed133f2eacd45778aad72547ffd0dedb355854
ahwz <andreas.zincke@gmx.at>
2025-01-22 14:45:53 +0100

    fix edgecase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -58,7 +58,6 @@ public class Main {
         System.out.println("Target is : " + target);
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i).equals(list.get(j))) continue;
                 int distance = Math.abs(list.get(i) + list.get(j) - target);
                 int difference = Math.abs(list.get(i) - list.get(j));
                 Pair pair = new Pair(new Integer[]{list.get(i), list.get(j)}, distance, difference);
